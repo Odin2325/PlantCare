261c046c898d42cdc59451ad758563ec15c021c2
Nicolas Arevalo Hoelscher <34999938+Odin2325@users.noreply.github.com>
2026-09-19 12:11:47 +0200

    Add integration test service extensions

--- PlantCare.Api.IntegrationTests/Infrastructure/PlantCareApiFactory.cs
+++ PlantCare.Api.IntegrationTests/Infrastructure/PlantCareApiFactory.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using PlantCare.Infrastructure.Persistence;
 
